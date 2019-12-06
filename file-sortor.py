05c58ceae22343a2d657ae11d58096dacea741b9
Mitesh <46081187+Mitesh2499@users.noreply.github.com>
2019-12-06 07:37:50 +0530

    Add files via upload

---
+++ file-sortor.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+os.chdir("E:\downloads")
+print(os.getcwd())
+
+files = os.listdir()
+extentions = {
+    "images": [".jpg", ".png", ".jpeg", ".gif"],
+    "videos": [".mp4", ".mkv"],
+    "musics": [".mp3", ".wav"],
+    "zip": [".zip", ".tgz", ".rar", ".tar"],
+    "documents": [".pdf", ".docx", ".csv", ".xlsx", ".pptx", ".doc", ".ppt", ".xls"],
+    "setup": [".msi", ".exe"],
+    "programs": [".py", ".c", ".cpp", ".php", ".C", ".CPP"],
+    "design": [".xd", ".psd"]
+
+
+}
+
+
+def sorting(file):
+    keys = list(extentions.keys())
+    for key in keys:
+        for ext in extentions[key]:
+            # print(ext)
+            if file.endswith(ext):
+                return key
+
+
+for file in files:
+    dist = sorting(file)
+    if dist:
+        try:
+            shutil.move(file, "../download-sorting/" + dist)
+        except:
+            print(file + " is already exist")
+    else:
+        try:
+            shutil.move(file, "../download-sorting/others")
+        except:
+            print(file + " is already exist")
